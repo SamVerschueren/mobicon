df64070568e14e0ef1575c75c5ca5c0ea7737a89
Sam Verschueren <sam.verschueren@gmail.com>
2015-12-24 08:30:26 +0100

    write to a tempfile in the tests

--- test.js
+++ test.js
@@ -1,35 +1,38 @@
 import test from 'ava';
+import path from 'path';
 import pathExists from 'path-exists';
-import del from 'del';
+import tempfile from 'tempfile';
 import fn from './';
 
-test.after(async () => {
-	await del(['android', 'ios', 'bb10']);
-});
-
 test('android', async t => {
-	await fn('./fixtures/icon.png', {platform: 'android', dest: 'android'});
+	const dest = tempfile();
 
-	t.true(pathExists.sync('android/drawable/icon.png'));
-	t.true(pathExists.sync('android/drawable-ldpi/icon.png'));
-	t.true(pathExists.sync('android/drawable-mdpi/icon.png'));
-	t.true(pathExists.sync('android/drawable-hdpi/icon.png'));
-	t.true(pathExists.sync('android/drawable-xhdpi/icon.png'));
-	t.true(pathExists.sync('android/drawable-xxhdpi/icon.png'));
-	t.true(pathExists.sync('android/drawable-xxxhdpi/icon.png'));
+	await fn('./fixtures/icon.png', {platform: 'android', dest: dest});
+
+	t.true(pathExists.sync(path.join(dest, 'drawable/icon.png')));
+	t.true(pathExists.sync(path.join(dest, 'drawable-ldpi/icon.png')));
+	t.true(pathExists.sync(path.join(dest, 'drawable-mdpi/icon.png')));
+	t.true(pathExists.sync(path.join(dest, 'drawable-hdpi/icon.png')));
+	t.true(pathExists.sync(path.join(dest, 'drawable-xhdpi/icon.png')));
+	t.true(pathExists.sync(path.join(dest, 'drawable-xxhdpi/icon.png')));
+	t.true(pathExists.sync(path.join(dest, 'drawable-xxxhdpi/icon.png')));
 });
 
 test('ios', async t => {
-	await fn('./fixtures/icon.png', {platform: 'ios', dest: 'ios'});
+	const dest = tempfile();
 
-	t.true(pathExists.sync('ios/icon.png'));
+	await fn('./fixtures/icon.png', {platform: 'ios', dest: dest});
+
+	t.true(pathExists.sync(path.join(dest, 'icon.png')));
 });
 
 test('bb10', async t => {
-	await fn('./fixtures/icon.png', {platform: 'blackberry10', dest: 'bb10'});
+	const dest = tempfile();
 
-	t.true(pathExists.sync('bb10/icon-90.png'));
-	t.true(pathExists.sync('bb10/icon-96.png'));
-	t.true(pathExists.sync('bb10/icon-110.png'));
-	t.true(pathExists.sync('bb10/icon-144.png'));
+	await fn('./fixtures/icon.png', {platform: 'blackberry10', dest: dest});
+
+	t.true(pathExists.sync(path.join(dest, 'icon-90.png')));
+	t.true(pathExists.sync(path.join(dest, 'icon-96.png')));
+	t.true(pathExists.sync(path.join(dest, 'icon-110.png')));
+	t.true(pathExists.sync(path.join(dest, 'icon-144.png')));
 });
